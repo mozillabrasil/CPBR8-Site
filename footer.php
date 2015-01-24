8d4455269de50649fdb8f6672e9ade010b5efcc5
SamuelMoraesF <samuel@samuelmoraes.com>
2015-01-24 17:08:30 -0200

    Facebook Like Box in Iframe(responsive)

--- footer.php
+++ footer.php
@@ -41,7 +41,7 @@
 							<h3 class="title margin-bottom-20">MozBR no Facebook</h3>
 
 							<div class="recent-posts">
-								<div class="fb-like-box" data-href="https://www.facebook.com/mozillabrasil" data-colorscheme="dark" data-show-faces="true" data-header="false" data-stream="false" data-show-border="true"></div>
+                  <iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fmozillabrasil&amp;width=500&amp;colorscheme=dark&amp;show_faces=true&amp;header=false&amp;stream=false&amp;show_border=false&amp;appId=435972309796449" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100%; height:258px;" allowTransparency="true"></iframe>
 							</div>
 						</div>
 
--- header.php
+++ header.php
@@ -43,14 +43,6 @@
     <![endif]-->
   </head>
   <body>
-    <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=435972309796449&version=v2.0";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
   	<!-- navbar -->
   	<div class="navbar-wrapper">
   		<div class="container">
