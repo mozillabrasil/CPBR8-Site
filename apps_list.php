6c34af6f37a1b0463999e6f159feaca23a151b1a
SamuelMoraesF <samuel@samuelmoraes.com>
2015-02-04 19:02:22 -0200

    Needed Apps table

---
+++ apps_list.php
@@ -0,0 +1,469 @@
+<?php
+
+$aplicativos = [
+
+  [
+  "title"       => "Wikipedia",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Instagram",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "LinkedIn",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Skype",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Contact importer from Google and other providers",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Evernote",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Pocket",
+  "description" => "IRC they have some mobile-friendly website, though not offline what would be nice",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "File sharing client",
+  "description" => "Dropbox, Sparkleshare, Owncloud, ubuntuone...",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "PayPal",
+  "description" => "Mobile payment solutions.",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "BitCoin",
+  "description" => "Must support QR code scan and generation and handle bitcoin",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Ebook reader",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "epub and mobi support",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "YouTube / Vimeo",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Office solution",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Spotify",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Free-as-in-freedom social networks",
+  "description" => "Clientes de redes sociais como o GNU Social, Diaspora...",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Tune In",
+  "description" => "App to listen to radio stations from around the world.",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Translator",
+  "description" => "Tradutor de conteúdo.",
+  "example"       => "http://translate.google.com",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Voice recorder",
+  "description" => "",
+  "example"       => "TinyVox",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"             => "Drawing / Paint app",
+  "description"       => "",
+  "type"            => "p2"
+  ],
+
+  [
+  "title"       => "Image processing programs",
+  "description" => "Editores de fotos com filtros, moduras, efeitos...",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Advanced camera apps",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Dictionary",
+  "description" => "",
+  "example"       => "Wiktionary",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Encyclopedia",
+  "description" => "",
+  "example"       => "Wikipedia",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Stocks",
+  "description" => "Stocks application with news feed and charts",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Currency / Converter",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Financial market apps",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Reddit",
+  "description" => "They have a somekind of responsive design, more work on that and a manifests and it's done.",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Tumblr",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "TripAdvisor",
+  "description" => "app for the webpage tripadvisor- get hotels, flights, places, reviews and rates for them - easy to book, to vplan with it. ",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "GoodReads",
+  "description" => "A Social app for book readers - rate, explore book, share notes, quotes, impressions, challenge yourslef",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "WikiVoyage",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Shazam/Sound Hound",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Vidyo",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "IRC Client",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "WebRTC",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Pinterest",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "eBay",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "WordPress",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Flickr",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "GPS Navigator",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Flipboard",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Imdb",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Bugzilla / GitHub bug tracker client",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Cooking Recipe",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Zip archiver",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "File & folder manager",
+  "description" => "",
+  "type"        => "p2"
+  ],
+
+  [
+  "title"       => "Mesh network app",
+  "description" => "",
+  "type"        => "p3"
+  ],
+
+  [
+  "title"       => "Quora",
+  "description" => "",
+  "type"        => "p3"
+  ],
+
+  [
+  "title"       => "Speedtest",
+  "description" => "",
+  "type"        => "p3"
+  ],
+
+  [
+  "title"       => "ReMo Companion",
+  "description" => "",
+  "type"        => "p3"
+  ],
+
+  [
+  "title"       => "K-9 Mail client",
+  "description" => "",
+  "type"        => "p3"
+  ],
+
+  [
+  "title"       => "Usenet client",
+  "description" => "NewsGroup",
+  "type"        => "p3"
+  ],
+
+  [
+  "title"       => "Instrument Tuner",
+  "description" => "",
+  "type"        => "p3"
+  ],
+
+  [
+  "title"       => "Threema",
+  "description" => "Secure WhatsApp alternative",
+  "example"     => "http://threema.ch/en/",
+  "type"        => "p3"
+  ],
+
+  [
+  "title"       => "Mail Client with builtin GPG/PGP support",
+  "description" => "",
+  "type"        => "p3"
+  ],
+
+  [
+  "title"       => "MindMaps",
+  "description" => "",
+  "type"        => "p3"
+  ],
+
+  [
+  "title"       => "Photo Grid / Collage",
+  "description" => "",
+  "type"        => "p3"
+  ],
+
+  [
+  "title"       => "BugDroid/ Bugzilla",
+  "description" => "",
+  "type"        => "p3"
+  ]
+
+];
+
+$games = [
+  [
+  "title"       => "Angry Birds Like",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Bridge Building (Bridge Architect, X Construct)",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Chess",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Classic Card Games",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Emulators",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Game/Logo Quiz",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Memory games",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Piano",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Pool Master Pro",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Tower Defense games",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Robo Defense",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Rhythm games",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Sudoku",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Words With Friends",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Japanese puzzles",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Kids' apps",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+  [
+  "title"       => "Puzzle",
+  "description" => "",
+  "type"        => "p1"
+  ],
+
+];
